88c84e7662d69464016e465af600be43618a65f7
snarkusius <marcus@lengquist.se>
2020-10-19 15:09:40 +0200

    uppgift 6

---
+++ 6/script.js
@@ -0,0 +1,8 @@
+function siSameuser(objekt1,objekt2) {
+    return objekt1.email == objekt2.email
+}
+
+//test
+let user1 = {email:"supp@supp.com"}
+let user2 = {email:"supp2@supp.com"}
+console.log(siSameuser(user1,user2))
